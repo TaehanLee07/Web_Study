1a2b13491959d178fb820e1ef10e2de5ee4f784b
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-02 18:02:33 +0900

    Create Chk03.js

---
+++ PassWordChecker/Checkers/Chk03/Chk03.js
@@ -0,0 +1,13 @@
+function chkPW(){
+
+var reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+
+var pw = $("#password").val();
+
+if(false === reg.test(pw)) {
+alert('비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.');
+}else {
+console.log("통과");
+}
+
+}
